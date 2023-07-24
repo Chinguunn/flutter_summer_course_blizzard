4323bd15b891385b8bdb2d088aafb0a0dc60d409
Blizzard-11 <blizzard-11@Admin-11s-iMac.local>
2023-07-24 13:07:48 +0800

    wordfid app

--- wordfind_app/lib/welcome_page.dart
+++ wordfind_app/lib/welcome_page.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:wordfind_app/Gradien_letter.dart';
 
 import 'gradient_text.dart';
 
-
-class WelcomePage extends StatelessWidget{
+class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: GradientText('GAME', 31.6));
-  }
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            GradientLetter(letter: 'W'),
+            GradientLetter(letter: 'O'),
+            GradientLetter(letter: 'R'),
+            GradientLetter(letter: 'D'),
+          ],
+        ),
+      ),
+      floatingActionButton: Container(
+        width: 310,
+        height: 60,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [Color(0xFFE86B02) , Color(0xFFFA9541)],
 
+          ),
+          borderRadius: BorderRadius.circular(25)
+        ),
+      ),
+    );
+  }
 }
