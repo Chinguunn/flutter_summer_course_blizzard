4cbb3a01f525f238dbfe46f649296077468fc164
Blizzard-11 <blizzard-11@Admin-11s-iMac.local>
2023-07-31 13:03:53 +0800

    wordfind app

--- wordfind_app/lib/welcome_page.dart
+++ wordfind_app/lib/welcome_page.dart
@@ -8,19 +8,34 @@ class WelcomePage extends StatelessWidget {
     return Scaffold(
         backgroundColor: Color(0xFFFBF5F2),
         body: Container(
-          decoration: BoxDecoration(image: DecorationImage(image: AssetImage('image/back1.png'), fit:BoxFit.cover)),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/back1.png'),
+                  fit: BoxFit.cover)),
           child: Column(
-           mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-           const Padding( padding: EdgeInsets.only(top: 200), ),
-              Expanded(child: Column(
-                children: [Row()],
-              )),
+              const Padding(
+                padding: EdgeInsets.only(top: 200),
+              ),
+              Expanded(
+                  child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GradientLetter(letter: 'w'),
+                      GradientLetter(letter: 'O'),
+                      GradientLetter(letter: 'R'),
+                      GradientLetter(letter: 'd'),
+                    ],
+                  ),
+                  GradientText('GAME', 31.6),
+                  Image.asset('assets/iCodeGuy.png' ),
 
-              GradientLetter(letter: 'w'),
-              GradientLetter(letter: 'O'),
-              GradientLetter(letter: 'R'),
-              GradientLetter(letter: 'd'),
+                   Expanded(child: GradientText('READY?', 25.0))
+                ],
+              )),
             ],
           ),
         ),
@@ -43,7 +58,7 @@ class WelcomePage extends StatelessWidget {
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(25))),
               child: Text(
-                'play',
+                'PLAY',
                 style: TextStyle(
                     fontFamily: 'Nunito',
                     fontSize: 24,
