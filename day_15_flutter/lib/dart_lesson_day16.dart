ab21a9b7a5ed9f48f1045eee3feea4ab124b7acc
Blizzard-11 <blizzard-11@Admin-11s-iMac.local>
2023-07-31 12:08:18 +0800

    add day 16 statefull

---
+++ day_15_flutter/lib/dart_lesson_day16.dart
@@ -0,0 +1,16 @@
+int a = 16;
+
+void main() {
+  if (a > 15) {
+    print('number is less than $a');
+  } else {
+    print('number is bigger than $a');
+  }
+  a > 15
+      ? print('number is less than $a')
+      : print('number is bigger than $a');
+
+bool isBigger= a>15? false :true;
+print (isBigger);
+
+}
