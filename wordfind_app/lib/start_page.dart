b69f97aacb187c779b74c4ef4272fed4e79d4eb7
Blizzard-11 <blizzard-11@Admin-11s-iMac.local>
2023-08-09 13:04:05 +0800

    word find app

--- wordfind_app/lib/start_page.dart
+++ wordfind_app/lib/start_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:wordfind_app/Models/user_model.dart';
 import 'package:wordfind_app/gradient_text.dart';
 import 'input_field.dart';
 
-class StartPage extends StatelessWidget {
+User newUser = User("Guest", 0);
+
+class StartPage extends StatefulWidget {
   const StartPage({super.key});
 
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
 
-
+class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,15 +35,17 @@ class StartPage extends StatelessWidget {
         height: 50.0,
         decoration: BoxDecoration(
             image: DecorationImage(
-                image: AssetImage('assets/back2.png '),fit: BoxFit.cover )),
+                image: AssetImage('assets/back2.png '), fit: BoxFit.cover)),
         child: Center(
           child: Column(
             children: [
               Padding(padding: EdgeInsets.only(top: 50)),
               Image.asset('assets/iCodeGuyHead.png'),
               Padding(padding: EdgeInsets.only(top: 20)),
-              GradientText('Player Name', 20.0, ),
-
+              GradientText(
+                'Player Name',
+                20.0,
+              ),
               Padding(padding: EdgeInsets.only(top: 20))
             ],
           ),
@@ -47,40 +55,50 @@ class StartPage extends StatelessWidget {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
+
+  _createUser(String userName) {
+    setState(() {
+      newUser.userName;
+    });
+  }
 }
 
 class StartButton extends StatelessWidget {
-  const StartButton(String s, {super.key});
+  const StartButton(User newUser, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 310,
-        height: 60,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.centerRight,
-              end: Alignment.centerLeft,
-              colors: [
-                Color(0xFFE86B02),
-                Color(0xFFFA9541),
-              ],
+    if (newUser.userName == 'Guest') {
+      return Container();
+    } else {
+      return Container(
+          width: 310,
+          height: 60,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft,
+                colors: [
+                  Color(0xFFE86B02),
+                  Color(0xFFFA9541),
+                ],
+              ),
+              borderRadius: BorderRadius.circular(25)),
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'Start',
+              style: TextStyle(
+                  fontFamily: 'Nunito',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700),
             ),
-            borderRadius: BorderRadius.circular(25)),
-        child: ElevatedButton(
-          onPressed: (){},
-          child: Text(
-            'Start',
-            style: TextStyle(
-                fontFamily: 'Nunito',
-                fontSize: 24,
-                fontWeight: FontWeight.w700),
-          ),
-          style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25))),
-        ));
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25))),
+          ));
+    }
   }
 }
